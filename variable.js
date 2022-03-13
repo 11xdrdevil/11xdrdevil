e4f28cd2798ab2ded8c1508a23c517bc55918744
MD <11xdrdevil@gmail.com>
2022-03-14 00:40:56 +0530

    Added colour picker & bootstrap

--- variable.js
+++ variable.js
@@ -1,16 +1,11 @@
 // Get the root element
 var r = document.querySelector(':root');
     
-// Create a function for getting a variable value
-function myFunction_get() {
-  // Get the styles (properties and values) for the root
-  var rs = getComputedStyle(r);
-  // Alert the value of the --blue variable
-  alert("The value of --blue is: " + rs.getPropertyValue('--blue'));
-}
+setInterval(()=>{
+    myFunction_SetColourAsElementColour()
+}, 200);
 
-// Create a function for setting a variable value
-function myFunction_set(ColorName) {
-  // Set the value of variable --blue to another value (in this case "lightblue")
-  r.style.setProperty('--themeColor', ColorName);
+function myFunction_SetColourAsElementColour() {
+  let color = document.getElementById('colourPicker').value;
+  r.style.setProperty('--themeColor', color);
 }
